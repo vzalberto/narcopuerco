55c873b8602c7e633ad75605d6cf7702ede5a432
Ubuntu <ubuntu@ip-172-31-22-103.us-west-2.compute.internal>
2014-06-20 01:27:59 +0000

    does the job

---
+++ hunt.js
@@ -0,0 +1,159 @@
+var Twit = require('twit')
+, https = require('https')
+, fs = require('fs')
+, config = require('./config.js')
+
+var t = new Twit({
+
+consumer_key : CONSUMER_KEY 
+, consumer_secret : CONSUMER_SECRET 
+, access_token: ACCESS_TOKEN
+, access_token_secret: ACCESS_TOKEN_SECRET
+
+})
+
+function saveToFile(tweet)
+{	
+	filename = __dirname + "/tuits/" + tweet.user.screen_name + tweet.id.toString();
+	data = tweet.id_str+','+tweet.user.screen_name+','+tweet.text+'\n'
+
+	
+	fs.writeFile(filename, data, function (err){
+		if(err) throw err;
+		console.log("Tweet archivado");
+	});
+}
+
+function printTweet(tweet){
+	console.log(
+	'\nUser: @' + tweet.user.screen_name
+	+ '\nText: ' + tweet.text
+	+ '\nid: ' + tweet.id
+	+ '\nid_str: ' + tweet.id_str
+	+ '\nHora: ' + tweet.created_at
+	+ '\n\n'
+	);
+	}
+
+function isThisRT(tuit){
+	if(tuit.indexOf("RT ") == 0)
+		return true;
+	else
+		return false;
+	}
+
+function isThisRevendedor(tuit){
+	if(tuit.indexOf("vendo") == 0)
+		return true;
+	else if(tuit.indexOf("me sobra") == 0)
+		return true;
+	else if(tuit.indexOf("Vendo") == 0)
+		return true;
+	else
+		return false;
+	}
+
+var evento = process.argv[2];
+keyword_list = [];
+if(process.argv.length > 3)
+{
+	keyword_list.push(process.argv[2]);
+	var i = 3;
+	while(i < process.argv.length)
+	{
+		//crear objeto de parametros
+		keyword_list.push(process.argv[i]);
+		i++;
+	}
+console.log(keyword_list.toString());
+}
+
+function generateWordList(keyword)
+	{
+		
+		var wordlist = [
+
+		 	keyword + ' boletos',
+			keyword + ' boleto',	
+			keyword + ' regalaremos',
+			keyword + ' regale',
+			keyword + ' regalare',
+			keyword + ' regalar',
+			keyword + ' pendientes',
+			keyword + ' pase',
+			keyword + ' pases',
+			keyword + ' ganar',
+			keyword + ' abono',
+			keyword + ' entradas',
+			keyword + ' entrada',
+			keyword + ' tengo',
+			keyword + ' tenemos',
+			keyword + ' cortesia',
+			keyword + ' cortesias',
+			keyword + ' sortear',
+			keyword + ' sortearemos',
+			keyword + ' invitacion',
+			keyword + ' invitaciones',
+			keyword + ' pulsera',
+			keyword + ' pulseras',
+			keyword + ' rifa',
+			keyword + ' rifar',
+			keyword + ' rifare',
+			keyword + ' rifaremos',
+	
+			'tortapuerca'
+
+		];
+
+		return wordlist;
+
+	};
+
+
+function generateParameterArray(keyword)
+	{
+		var paramArray = [];
+		//how many keywords?
+		var i = 0;
+		while(i < keyword.length)
+		{
+			paramArray = paramArray.concat(generateWordList(keyword[i]));
+			i++;
+		}
+		return paramArray;
+	};	
+
+wordlist = generateParameterArray(keyword_list);
+
+var stream = t.stream('statuses/filter',{
+		
+	track: wordlist 
+
+	 })
+
+var d = new Date();
+
+//Comienzo del script
+//t.post('statuses/update', {status: 'andas chido @paranoidhominid  '+ d.toDateString()}, function(err, reply){});
+console.log("Cazando boletos de " + evento + "\n" + d.toDateString());
+
+
+stream.on('tweet', function(tweet){
+	
+	printTweet(tweet);
+//	saveToFile(tweet);
+	if((tweet.retweet_count > 0) ||(isThisRT(tweet.text)) || isThisRevendedor(tweet.text)){console.log('no procede')}
+	else{
+		t.post('statuses/update',
+		{ 
+		status: 'hey @paranoidhominid ira ve https://twitter.com/'
+			 + tweet.user.screen_name 
+			 + '/status/' 
+			 + tweet.id_str 
+		},
+			function(err,reply){}
+		)
+	}
+})
+
+
